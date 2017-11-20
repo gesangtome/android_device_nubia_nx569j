2440ebd4ec7e70522bb2fa2ac16ba891e17c66f1
gesangtome <gesangtome@foxmail.com>
2017-11-20 10:41:43 +0800

    About the restoration of FMRadio
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

--- FMRadio/Android.mk
+++ FMRadio/Android.mk
@@ -1,40 +1,92 @@
-LOCAL_PATH:= $(call my-dir)
+# Copyright (c) 2017, 格桑花給我的. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+LOCAL_PATH := $(call my-dir)
+
+# =================================================
+# make the qcom.fmradio.jar <-- <gesangtome@189.cn>
+# =================================================
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := qcom.fmradio
+LOCAL_MODULE_OWNER := nubia
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+# =========================================
+# make the NubiaFM <-- <gesangtome@189.cn>
+# =========================================
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := src/com/caf/fmradio/CommaSeparatedFreqFileReader.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FMAdapterApp.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FMMediaButtonIntentReceiver.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FMRadio.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FMRadioService.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FmSharedPreferences.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FMStats.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/FmTags.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/GetNextFreqInterface.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/HorizontalNumberPicker.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/PresetList.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/PresetStation.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/Settings.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/StationListActivity.java
-LOCAL_SRC_FILES += src/com/caf/fmradio/IFMRadioService.aidl
-LOCAL_SRC_FILES += src/com/caf/fmradio/IFMRadioServiceCallbacks.aidl
-LOCAL_SRC_FILES += src/com/caf/fmradio/IFMTransmitterService.aidl
-LOCAL_SRC_FILES += src/com/caf/fmradio/IFMTransmitterServiceCallbacks.aidl
-
-ifeq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 11 ))" )))
-LOCAL_SRC_FILES +=  $(call all-java-files-under, src/com/caf/hc_utils)
-else
-LOCAL_SRC_FILES +=  $(call all-java-files-under, src/com/caf/utils)
-endif
-LOCAL_PACKAGE_NAME := FMRADIO
+LOCAL_MODULE := NubiaFM
+LOCAL_MODULE_OWNER := nubia
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_JNI_SHARED_LIBRARIES := \
+LOCAL_ADDITIONAL_DEPENDENCIES := \
     libqcomfm_jni \
-    libfmjni
+    libfmjni \
 
-LOCAL_JAVA_LIBRARIES := \
+LOCAL_ADDITIONAL_DEPENDENCIES += \
     qcom.fmradio
 
-include $(BUILD_PACKAGE)
+include $(BUILD_PREBUILT)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Make the dependencies
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+include $(CLEAR_VARS)
+LOCAL_MODULE := libfmjni
+LOCAL_MODULE_OWNER := nubia
+LOCAL_SRC_FILES_64 := arm64/libfmjni.so
+LOCAL_SRC_FILES_32 := arm/libfmjni.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libqcomfm_jni
+LOCAL_MODULE_OWNER := nubia
+LOCAL_SRC_FILES_64 := arm64/libqcomfm_jni.so
+LOCAL_SRC_FILES_32 := arm/libqcomfm_jni.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
