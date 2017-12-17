ec99901988e0ce517ec8b025c8d1d59dcef48630
gesangtome <gesangtome@foxmail.com>
2017-12-17 11:31:45 +0800

    NX569J: Delete the default ring configuration item,         * which is provided by vendor/sm/config/sm_audio.mk
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

---
+++ sounds/AllAudio.mk
@@ -0,0 +1,51 @@
+# Copyright (c) 2017, 格桑花給我的. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+PRODUCT_COPY_FILES += \
+    device/nubia/nx569j/sounds/alarms/Country_sunshine.ogg:system/media/alarms/Country_sunshine.ogg \
+    device/nubia/nx569j/sounds/alarms/Flush_Of_Dawn.ogg:system/media/alarms/Flush_Of_Dawn.ogg \
+    device/nubia/nx569j/sounds/alarms/Glassy_Lustre.ogg:system/media/alarms/Glassy_Lustre.ogg \
+    device/nubia/nx569j/sounds/alarms/Lights.ogg:system/media/alarms/Lights.ogg \
+    device/nubia/nx569j/sounds/alarms/Little_Flickering.ogg:system/media/alarms/Little_Flickering.ogg \
+    device/nubia/nx569j/sounds/alarms/Mysterious_castle.ogg:system/media/alarms/Mysterious_castle.ogg \
+    device/nubia/nx569j/sounds/alarms/Nubia_mile.ogg:system/media/alarms/Nubia_mile.ogg \
+    device/nubia/nx569j/sounds/alarms/Nubia_mile_piano.ogg:system/media/alarms/Nubia_mile_piano.ogg \
+    device/nubia/nx569j/sounds/alarms/Nubia_mile_string.ogg:system/media/alarms/Nubia_mile_string.ogg \
+    device/nubia/nx569j/sounds/alarms/Nubia_mile_zither.ogg:system/media/alarms/Nubia_mile_zither.ogg \
+    device/nubia/nx569j/sounds/alarms/Open_Forest.ogg:system/media/alarms/Open_Forest.ogg \
+    device/nubia/nx569j/sounds/alarms/peaceful_wave.ogg:system/media/alarms/peaceful_wave.ogg \
+    device/nubia/nx569j/sounds/alarms/Rain_rhythm.ogg:system/media/alarms/Rain_rhythm.ogg \
+    device/nubia/nx569j/sounds/alarms/Ramble_over_country.ogg:system/media/alarms/Ramble_over_country.ogg \
+    device/nubia/nx569j/sounds/alarms/Sound_Of_The_Sea.ogg:system/media/alarms/Sound_Of_The_Sea.ogg
+
+PRODUCT_COPY_FILES += \
+    device/nubia/nx569j/sounds/notifications/Funtouch.ogg:system/media/notifications/Funtouch.ogg \
+    device/nubia/nx569j/sounds/notifications/MusicBox.ogg:system/media/notifications/MusicBox.ogg
+
+PRODUCT_COPY_FILES += \
+    device/nubia/nx569j/sounds/notifications/Harp.ogg:system/media/notifications/Harp.ogg \
+    device/nubia/nx569j/sounds/notifications/Sunrise_View.ogg:system/media/notifications/Sunrise_View.ogg
