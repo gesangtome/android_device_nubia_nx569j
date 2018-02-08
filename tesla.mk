ee3a6734996ed75e7d463b7a5a5fb7663a1f2dd1
Flowertome <gesangtome@foxmail.com>
2018-02-08 23:23:08 +0800

    Modify the makefile call path
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- tesla.mk
+++ tesla.mk
@@ -29,8 +29,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx569j/device.mk)
