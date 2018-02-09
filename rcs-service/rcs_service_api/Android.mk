bc32e7ca11003192a3403d024a2fe570089e183e
Flowertome <gesangtome@foxmail.com>
2018-02-09 15:37:05 +0800

    Merge branch 'LA.BR.1.3.5_rb1.9' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/rcs-service into tesla-6.0
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- rcs-service/rcs_service_api/Android.mk
+++ rcs-service/rcs_service_api/Android.mk
@@ -3,10 +3,9 @@ LOCAL_PATH := $(call my-dir)
 # Make rcs_service_api.jar
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, ../COMMON/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../rcs_ui_common/src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := rcs_service_api
-LOCAL_ADDITIONAL_DEPENDENCIES := rcs_service_api.xml
 LOCAL_STATIC_JAVA_LIBRARIES := rcs_service_aidl_static com.android.vcard
 LOCAL_JAVA_LIBRARIES := telephony-common
 include $(BUILD_JAVA_LIBRARY)
