fa336883d70228b218944bfd9071e3d41a9f7fa2
Flowertome <gesangtome@foxmail.com>
2018-02-12 21:36:19 +0800

    Update the makefile: device.mk
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- device.mk
+++ device.mk
@@ -22,3 +22,7 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_nx569j
 PRODUCT_DEVICE := nx569j
+
+# Dalvik / HWUI
+$(call inherit-product, device/nubia/nx569j/frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, device/nubia/nx569j/frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
