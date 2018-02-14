735d766eb34e6aae025b93f14f98b3e71388804f
Flowertome <gesangtome@foxmail.com>
2018-02-14 20:21:51 +0800

    Include generic CAF packages
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- aosp_nx569j.mk
+++ aosp_nx569j.mk
@@ -29,6 +29,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Generic CAF packages
+include device/qcom/common/common.mk
+
 # Inherit device configuration
 $(call inherit-product, device/nubia/nx569j/device.mk)
 
