3f27e6ac21bd40cec2020429454fc08962157be5
Flowertome <gesangtome@foxmail.com>
2018-02-14 19:38:47 +0800

    Update the makefile: android.mk
     * Remove the judgment process related to module: SMD
     * Combine liblight to drive to this repository
     * Remove the conditional judgment process
    
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- hardware/display/Android.mk
+++ hardware/display/Android.mk
@@ -1,26 +1,32 @@
-ifeq ($(call is-board-platform-in-list, msm8996),true)
-    TARGET_USES_SDM = true
-else
-    TARGET_USES_SDM = false
-endif
+# Copyright (c) 2018, Flowertome. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-display-hals := libgralloc libcopybit libmemtrack libqservice libqdutils
-display-hals += hdmi_cec
+target-hals := hdmi_ce libcopybit libgralloc libhdmi libhwcomposer liblight libmemtrack liboverlay libqdutils libqservice
 
-ifeq ($(TARGET_USES_SDM), true)
-    sdm-libs := sdm/libs
-    display-hals += $(sdm-libs)/utils $(sdm-libs)/core $(sdm-libs)/hwc
-else
-    display-hals += libgenlock libhwcomposer liboverlay libhdmi
-endif
 
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
-display-hals += liblight
-endif
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
-endif
+include $(call all-named-subdir-makefiles,$(target-hals))
+
