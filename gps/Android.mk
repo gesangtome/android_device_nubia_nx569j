489f4d6fb576f0bf8afbdf1332d3f600d6573061
Flowertome <gesangtome@foxmail.com>
2018-02-28 21:45:34 +0800

    Update the repository: gps
    * In the previous repository
    * The build system ignored the compilation of GPS
    * Because of the rule error of the makefile
    
    * After the Flag is modified
    * The build system will compile the GPS module properly now
    
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -25,4 +25,6 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -1,17 +1,42 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+# Copyright (c) 2018, Flowertome. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ifneq ($(BOARD_USES_QCOM_HARDWARE),)
 
 LOCAL_PATH := $(call my-dir)
 
 # add RPC dirs if RPC is available
 ifneq ($(TARGET_NO_RPC),true)
-
 GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api-rpc-50001/
-
 endif #TARGET_NO_RPC
 
 GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
 
-#call the subfolders
+# call the subfolders
 include $(addsuffix Android.mk, $(GPS_DIR_LIST))
 
-endif#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+endif #BOARD_USES_QCOM_HARDWARE
