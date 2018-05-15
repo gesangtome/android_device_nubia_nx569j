91d029480c8fbdda9addbafe5939394b76f5c350
Flowertome <gesangtome@foxmail.com>
2018-05-16 02:06:51 +0800

    Update the list of the HIDL(Binderized HAL) modules
    Signed-off-by: Flowertome <gesangtome@foxmail.com>

--- device.mk
+++ device.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera.msm8952 \
@@ -182,6 +183,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
@@ -286,7 +288,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2s.0-impl \
+    android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     lights.msm8952
 
